d6924140b7bcd9a8d55c4b24def189374edd373f
prajjwal98dubey <prajjwaldubey003@gmail.com>
2025-05-03 11:30:04 +0530

    updated the formatted time function

--- client/src/helpers/getFormattedTime.js
+++ client/src/helpers/getFormattedTime.js
@@ -1,22 +1,20 @@
 export const getTimeStamp = (time) => {
   const diffTime = Date.now() - time;
-  const seconds = diffTime / 1000;
-  if (Math.floor(seconds) === 0) {
+
+  let seconds = Math.floor(diffTime / 1000);
+  let minutes = Math.floor(diffTime / (1000 * 60));
+  let hours = Math.floor(diffTime / (1000 * 60 * 60));
+  let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  let months = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 30));
+  let year = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 30 * 12));
+  if (seconds === 0) {
     return "just now";
-  }
-  const minutes = seconds / 60;
-  if (Math.floor(minutes) === 0) {
-    return Math.round(seconds) + " seconds ago";
-  }
-  const hours = minutes / 60;
-  if (Math.floor(hours) === 0) {
-    return Math.round(minutes) + " minutes ago";
-  }
-  const day = hours / 24;
-  if (Math.floor(day) === 0) {
-    return Math.round(hours) + " hours ago";
-  }
-  return Math.round(day) + " day ago";
+  } else if (minutes === 0) return `${seconds} seconds ago.`;
+  else if (hours === 0) return `${minutes} minutes ago`;
+  else if (days === 0) return `${hours} hours ago`;
+  else if (months === 0) return `${days} days ago`;
+  else if (year === 0) return `${months} months ago`;
+  return `${year} year ago`;
 };
 
 export const timeDiffEndTimeToToday = (endTime) => {
